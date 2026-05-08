7ff88c00a187bbc5892c4e7e8bd8ee42e9d61bad
ivannaspas11 <ivannaspas11@gmail.com>
2026-05-08 19:44:39 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx'; // Переконайтеся, що тут .jsx
+import './styles.css';
 
-import App from "./App.jsx";
-
-const rootElement = document.getElementById('root');
-const root = ReactDOM.createRoot(rootElement);
-root.render(<App />);
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
