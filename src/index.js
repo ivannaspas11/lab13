b2c3b60607239e63bfa376b7e584352e60b1243a
ivannaspas11 <ivannaspas11@gmail.com>
2026-05-08 17:19:33 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,6 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+root.render(<App />);
